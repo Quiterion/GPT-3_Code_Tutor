4e5120a4fe42c4c0b93dcf557901029f606e5c4a
Quiterion <quiterion12@gmail.com>
2021-09-03 22:39:11 +1000

    fix up info embed again lol

--- bot.py
+++ bot.py
@@ -6,7 +6,9 @@ import datetime
 import os
 import openai
 
-bot = commands.Bot(command_prefix='?', description="This is a GPT-3 derived bot made for answering programming questions.")
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='?', description="This is a GPT-3 derived bot made for answering programming questions.", intents=intents)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 token = os.getenv("DISCORD_API_KEY")
 
@@ -118,10 +120,10 @@ async def get_last(ctx):
 async def info(ctx):
     embed = discord.Embed(title=f"{ctx.guild.name}", description="", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
     embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
-    embed.add_field(name="Server Owner", value=f"{ctx.guild.owner.name}")
+    embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
     embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
     embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
-    embed.set_thumbnail(url=f"{str(ctx.guild.icon_url)}")
+    embed.set_thumbnail(url=f"{ctx.guild.icon_url}")
     #embed.set_thumbnail(url="https://pluralsight.imgix.net/paths/python-7be70baaac.png")
 
     await ctx.send(embed=embed)
