b6b1a2b20800919e4dfdbbb8ab87b7748fec1e9a
Quiterion <quiterion12@gmail.com>
2021-09-10 21:32:50 +1000

    Add cogs and support command

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import discord
 from discord.ext import commands
 import datetime
@@ -14,7 +13,6 @@ token = os.getenv("DISCORD_API_KEY")
 
 base_prompt = "I am a highly intelligent question answering bot specializing in computer programming. I possess advanced knowledge in bash, C, Java, and Python. If I do not know the answer to a question, I will respond with 'I don't know'. If a question is not about coding, I will not respond.\n\nQ: What is a froopy in Python?\nA: I don't know\n\nQ: How do you open a file in C?\nA: Use the fopen() function. The syntax is:\n`FILE *fopen(const char *filename, const char *mode)`\n\nQ: "
 
-last_prompt_dict = {}
 
 
 def check_completion_label(content_to_classify, author_id):
@@ -72,67 +70,92 @@ def check_completion_label(content_to_classify, author_id):
     return output_label
 
 
+
+
+
 # Commands
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+class Gpt3Commands(commands.Cog):
 
+    def __init__(self, bot):
+        self.bot = bot
+        self.last_prompt_dict = {}
 
-@bot.command(name='q')
-async def gpt3_ask(ctx, *, arg):
-    if len(arg) > 120:
-        await ctx.send("I'm sorry, this question is too large.")
-    else:
-        await ctx.channel.trigger_typing()
-        author_id = ctx.author.name + ctx.author.discriminator
-        full_prompt = base_prompt+last_prompt_dict.get(ctx.channel.id, '')+arg+"\nA:"
-        response = openai.Completion.create(
-          engine="davinci",
-          prompt=full_prompt,
-          temperature=0,
-          max_tokens=75,
-          top_p=1,
-          frequency_penalty=0,
-          presence_penalty=0.50,
-          stop=["\nQ:","\nA:"],
-          user=author_id
-        )
-        answer = response.choices[0].text
-
-        # Check for invalid output
-        if answer[1:].strip() == "" or check_completion_label(full_prompt, author_id) == "2":
-            await ctx.send("I have no answer to that question.")
+    @commands.command(name='q')
+    async def gpt3_ask(self, ctx, *, arg):
+        if len(arg) > 120:
+            await ctx.send("I'm sorry, this question is too large.")
         else:
-            # Store completion for context
-            last_prompt_dict[ctx.channel.id] = arg + "\nA:" + answer + "\n\nQ: "
-            await ctx.send(answer[1:])
-            if ctx.guild is not None:
-                print(f"Message sent in {ctx.guild.name}: {ctx.channel.name}")
-                print(f"Guild ID: {ctx.guild.id}")
-            elif ctx.channel.type == discord.ChannelType.private:
-                print(f"Message sent in DM to: {ctx.channel.recipient}")
+            await ctx.channel.trigger_typing()
+            author_id = ctx.author.name + ctx.author.discriminator
+            full_prompt = base_prompt+self.last_prompt_dict.get(ctx.channel.id, '')+arg+"\nA:"
+            response = openai.Completion.create(
+              engine="davinci",
+              prompt=full_prompt,
+              temperature=0,
+              max_tokens=75,
+              top_p=1,
+              frequency_penalty=0,
+              presence_penalty=0.50,
+              stop=["\nQ:","\nA:"],
+              user=author_id
+            )
+            answer = response.choices[0].text
+
+            # Check for invalid output
+            if answer[1:].strip() == "" or check_completion_label(full_prompt, author_id) == "2":
+                await ctx.send("I have no answer to that question.")
+            else:
+                # Store completion for context
+                self.last_prompt_dict[ctx.channel.id] = arg + "\nA:" + answer + "\n\nQ: "
+                await ctx.send(answer[1:])
+                if ctx.guild is not None:
+                    print(f"Message sent in {ctx.guild.name}: {ctx.channel.name}")
+                    print(f"Guild ID: {ctx.guild.id}")
+                elif ctx.channel.type == discord.ChannelType.private:
+                    print(f"Message sent in DM to: {ctx.channel.recipient}")
 
 
 
-@bot.command(name="last")
-async def get_last(ctx):
-    last = last_prompt_dict.get(ctx.channel.id)
-    if last:
-        await ctx.send("Q: " + last[:-3])
-    else:
-        await ctx.send("None")
+    @commands.command(name="last")
+    async def get_last(self, ctx):
+        last = self.last_prompt_dict.get(ctx.channel.id)
+        if last:
+            await ctx.send("Q: " + last[:-3])
+        else:
+            await ctx.send("None")
 
-@bot.command()
-async def info(ctx):
-    embed = discord.Embed(title=f"{ctx.guild.name}", description="", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
-    embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
-    embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
-    embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
-    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
-    embed.set_thumbnail(url=f"{ctx.guild.icon_url}")
-    #embed.set_thumbnail(url="https://pluralsight.imgix.net/paths/python-7be70baaac.png")
 
-    await ctx.send(embed=embed)
+class misc(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('pong')
+
+    @commands.command()
+    async def servinfo(self, ctx):
+        try:
+            embed = discord.Embed(title=f"{ctx.guild.name}", description="", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+            embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
+            embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
+            embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
+            embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
+            embed.set_thumbnail(url=f"{ctx.guild.icon_url}")
+            await ctx.send(embed=embed)
+        except:
+            await ctx.send("Oops! It seems like I don't have embed permissions.")
+
+    @commands.command()
+    async def support(self, ctx):
+        try:
+            embed = discord.Embed(title=f"Help support my API fees!", description="Each answer costs my maintainer a few cents to generate, which can add up quickly! Please help them cover the cost :)", timestamp=datetime.datetime.utcnow(), color=discord.Color.red())
+            embed.set_author(name=f"{self.bot.user.name}", icon_url=f"{self.bot.user.avatar_url}")
+            embed.add_field(name="Buy me a coffee with kofi", value="https://ko-fi.com/andromeda106", inline=False)
+            embed.set_thumbnail(url="https://uploads-ssl.webflow.com/5c14e387dab576fe667689cf/5ca5bf1dff3c03fbf7cc9b3c_Kofi_logo_RGB_rounded.png")
+            await ctx.send(embed=embed)
+        except:
+            await ctx.send("Please help support my API fees on kofi: https://ko-fi.com/andromeda106")
 
 # Events
 @bot.event
@@ -141,4 +164,7 @@ async def on_ready():
     print('Online')
 
 
-bot.run(token)
+if __name__ == "__main__":
+    bot.add_cog(Gpt3Commands(bot))
+    bot.add_cog(misc(bot))
+    bot.run(token)
